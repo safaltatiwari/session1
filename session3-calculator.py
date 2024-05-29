9f80a36ab65b5987330b3252d29aaf65b64537d1
safaltatiwari <230101@students.aih.nsw.edu.au>
2024-05-29 19:03:50 +1000

    Create session3-calculator.py

---
+++ session3-calculator.py
@@ -0,0 +1,14 @@
+first_num = int(input("Enter first number:"))
+sec_num = int(input("Enter second number:"))
+divisions = first_num / sec_num
+multi = first_num * sec_num
+addi = first_num + sec_num
+subs = first_num - sec_num
+expo = first_num ** sec_num
+modu = first_num % sec_num
+fdivi = first_num // sec_num
+print ("=============AIH CALCULATOR==========")
+print ("============Safalta=================")
+
+print("The Calculation is as follows:" , "Divisions:", divisions, "Multiplication:", multi, "Additions:", addi, "Subtraction:", subs, "Exponential:", expo, "Modulus:", modu, "Fdivisions:"
+ , fdivi)
