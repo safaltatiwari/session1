732144f4079b01ae340f87f91e685de5a2a02e49
safaltatiwari <230101@students.aih.nsw.edu.au>
2024-05-29 19:02:32 +1000

    Create session3-salestax.py

---
+++ session3-salestax.py
@@ -0,0 +1,11 @@
+ph = 1400 # initializing phone's value
+lap = 2000 # initializing laptop's value
+wtc = 1500 # initializing watch's value
+ap = 200 # initializing airpods value
+tv = 6000 # initializing tv's value
+st_ph =  ph*0.1
+st_lap = lap*0.2
+st_wtc = wtc* 0.08
+st_tv = tv*0.08
+print(st_ph, st_lap, st_wtc, st_tv)
+print (st_ph+ph, st_lap+lap, st_wtc+wtc, st_tv+tv)
